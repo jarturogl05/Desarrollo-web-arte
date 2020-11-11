95c2c986a2931c7b3f56fba3a52ad63d7344bf03
jarturogl05 <jarturogl05@gmail.com>
2020-11-11 15:54:10 -0600

    creación de contexto

--- Cliente/cliente/src/App.js
+++ Cliente/cliente/src/App.js
@@ -1,21 +1,54 @@
 import Login from './pages/login/login'
 import Home from './pages/home/home'
+import UserContext from "./context/userContext"
 
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 
 function App() {
+  const [userData, setUserData] = useState({
+    token: undefined,
+    token: undefined,
+  });
+
+  useEffect(() =>{
+    const checkLoggedIn = async () =>{
+      let token = localStorage.getItem("auth-token");
+
+      if(token === null){
+        localStorage.setItem("auth-token", "");
+        token = "";
+      }
+
+      const tokenRes = true; // reemplazar por llamada
+
+      if(tokenRes){
+
+        const userRes = {id:111, user: "muterk" } //reemplaar por llamada
+
+        setUserData({
+          token,
+          user: userRes,
+        });
+      }
+    };
+    
+    checkLoggedIn();
+  },[])
+
   return (
     <BrowserRouter>
-       <div className="App">
-        <Switch>
-          <Route exact path="/" component={Home}></Route>
-          <Route path="/login" component={Login}></Route>
-        </Switch>
-    </div>
+      <UserContext.Provider value={{ userData, setUserData }}>
+        <header/>
+        <div className="App">
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+            <Route path="/login" component={Login}></Route>
+          </Switch>
+        </div>
+      </UserContext.Provider>
     </BrowserRouter>
- 
   );
 }
 
---
+++ Cliente/cliente/src/context/userContext.js
@@ -0,0 +1,4 @@
+import {createContext} from "react"
+
+const context = createContext(null)
+export default context;
