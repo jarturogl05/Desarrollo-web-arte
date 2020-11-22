4a94ffad9e7ecfd66a6b64ec67c28d8d29cd6745
jarturogl05 <jarturogl05@gmail.com>
2020-11-21 22:01:49 -0600

    agrega contenido del post

--- Cliente/cliente/src/componens/publication/publication.js
+++ Cliente/cliente/src/componens/publication/publication.js
@@ -4,10 +4,19 @@ import './publication.css'
 
 function Publication(props) {
 
+    const postInfo = props.postInfo;
     return (
         <div className="publication">
-            <img src={props.postInfo.urlImage} alt="art"></img>
-            <h2>{props.postInfo.publicationName}</h2>
+            <img src={postInfo.urlImage} alt="art"></img>
+            <h2>{postInfo.publicationName}</h2>
+            <div className="publication_tags">
+            {
+                postInfo.tags.map(tag =>{
+                    return <a>{tag}</a>
+                })
+            }
+            </div>
+
         </div>
     )
 }
--- Cliente/cliente/src/pages/post/post.js
+++ Cliente/cliente/src/pages/post/post.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import {  useParams } from "react-router-dom";
 import NavBar from '../../componens/navbar/navbar'
-
+import Publication from '../../componens/publication/publication'
 
 function Post() {
     let { id } = useParams();
     console.log(id);
+    const postInfo = {id:1, publicationName: "Lofi cafe 👌", tags:["#City", "#Lofi"], urlImage:"https://i.redd.it/ytkkbowirm061.jpg"};
+
     return (
         
         <div>
             <NavBar></NavBar>
-            <h1>{id}</h1>
-
+            <Publication postInfo = {postInfo} ></Publication> 
         </div>
     )
 }
