b0c1699521312808ba540a67401155f09c6b9d1d
jarturogl05 <jarturogl05@gmail.com>
2020-11-22 21:53:28 -0600

    mostrar autor de una publicación

--- Cliente/cliente/src/componens/publication/publication.js
+++ Cliente/cliente/src/componens/publication/publication.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SocialButtons from "../social-buttons/social-buttons";
+import UserInfo from "../userInfo-publication/userInfo";
 import "./publication.css";
 
 function Publication(props) {
@@ -9,9 +10,7 @@ function Publication(props) {
       <img src={postInfo.urlImage} alt="art"></img>
 
       <div className="publication_info">
-
-        <div className="publication_info_metadata">
-            
+        <div className="publication_info_metadata">     
           <h2>{postInfo.publicationName}</h2>
           <div className="publication_info_tags">
             {postInfo.tags.map((tag) => {
@@ -21,8 +20,8 @@ function Publication(props) {
         </div>
 
         <SocialButtons></SocialButtons>
-
       </div>
+        <UserInfo></UserInfo>
     </div>
   );
 }
---
+++ Cliente/cliente/src/componens/userInfo-publication/userInfo.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './userInfo.css'
+import { useHistory } from "react-router-dom";
+
+
+import user from'./cat2.jpg'
+
+function UserInfo() {
+    const history = useHistory();
+
+    const handleUserClick = () =>{
+        history.push('/user');
+    }
+
+    return (
+        <div className="userInfo-container">
+            <img src={user} alt="user" onClick={handleUserClick}></img>
+            <h3 onClick={handleUserClick}>Muterk</h3>
+        </div>
+    )
+}
+
+export default UserInfo
