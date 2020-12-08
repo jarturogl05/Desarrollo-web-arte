aa1e5bfaffb2f3ec4a5f20c35f7791abbb425196
jarturogl05 <jarturogl05@gmail.com>
2020-12-08 12:09:36 -0600

    ruta para subir imagen

--- Servidor/src/routes/routes.js
+++ Servidor/src/routes/routes.js
@@ -2,10 +2,17 @@ const express = require('express');
 const router = express.Router();
 const path = require('path')
 const userController = require(`../controllers/user-controller`)
+const postService = require('../controllers/post-service')
 
+
+const multer = require('multer');
+const inMemoryStorage = multer.memoryStorage();
+const uploadStrategy = multer({storage: inMemoryStorage}).single('image');
 
 router.post('/login' , userController.login);
 router.post('/create' , userController.createUser);
 router.post('/authenticateToken', userController.authenticateToken);
+
+router.post('/upload', uploadStrategy, postService.createPost);
 
 module.exports = router;
