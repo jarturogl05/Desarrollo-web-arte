c02cb834472f1b11bf46a2a6b1e77c775dd87796
davisbd100 <davisbd100@gmail.com>
2020-12-11 13:35:42 -0600

    Change to references on screen

--- Cliente/cliente/src/services/profileServices.js
+++ Cliente/cliente/src/services/profileServices.js
@@ -15,11 +15,33 @@ async function getProfileInfo(username){
     try {
         const response = await fetch('http://localhost:4000/getUserProfileInfoByUsername', settings);
         const json = await response.json();
-        return json;
+        return json.data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+async function getAvailableCommissions(username){
+
+    const settings = {
+        method: 'POST',
+        headers: new Headers({
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }),
+
+        body: JSON.stringify({
+            'username': username
+        })
+    }
+
+    try {
+        const response = await fetch('http://localhost:4000/getAvailableCommissions', settings);
+        const json = await response.json();
+        return json.data;
     } catch (error) {
         console.log(error);
     }
 }
 
 
-export { getProfileInfo }
+export { getProfileInfo, getAvailableCommissions }
