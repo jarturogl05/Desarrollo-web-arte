c81c401c52cba9759ea906e736eea66fa843bca0
davisbd100 <davisbd100@gmail.com>
2020-12-18 11:55:00 -0600

    relationships between user and profile

--- Servidor/src/mongo/models/user.js
+++ Servidor/src/mongo/models/user.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username : {type: String, required: true, unique: true},
     password: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     confirmed: {type: Boolean, required: true},
