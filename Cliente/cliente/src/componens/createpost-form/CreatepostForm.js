19da9621157adf4011ff4bf8bda826dc3350ab14
jarturogl05 <jarturogl05@gmail.com>
2021-01-02 21:02:34 -0600

    arreglo de stilo del form

--- Cliente/cliente/src/componens/createpost-form/CreatepostForm.js
+++ Cliente/cliente/src/componens/createpost-form/CreatepostForm.js
@@ -3,34 +3,43 @@ import "./createpostForm.css";
 
 function CreatepostForm() {
   return (
-    <div>
-      <form className="postform">
-        <h2>Create new post</h2>
-        <label className='titlemessage'>Share your art</label>
-
+    <div className="postform">
+      <h2>Create new post</h2>
+      <label className="titlemessage">Share your art</label>
+      <form>
         <div className="postform-filecontainer">
-          <input id="file" type="file" accept="image/*" className='inputfile'></input>
+          <input
+            id="file"
+            type="file"
+            accept="image/*"
+            className="inputfile"
+          ></input>
           <label for="file">Select a Image</label>
         </div>
-
-        <div className='postform-labelscontainer'>
+        <div className='postform-formcontainer'>
+        <div className="postform-labelscontainer">
           <ul>
             <li>
-              <label for="name">Post Name:</label>
+              <label for="name">Title</label>
               <input id="name" type="text" autoComplete="off"></input>
             </li>
 
             <li>
-              <label for="tags">Tags: </label>
+              <label for="tags">Tags </label>
               <input id="tags" type="text"></input>
             </li>
 
             <li>
-              <label for="description">Tags: </label>
+              <label for="description">Description </label>
               <textarea id="description"></textarea>
             </li>
           </ul>
         </div>
+        <div className="postform-buttoncontainer ">
+          <button type="submit">Create Post</button>
+        </div>
+        </div>
+        
       </form>
     </div>
   );
