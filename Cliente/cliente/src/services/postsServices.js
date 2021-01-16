9e7b5dba5b9e5c91f3b6b96e676aa2d7f6a982bb
jarturogl05 <jarturogl05@gmail.com>
2021-01-15 21:28:06 -0600

    peticion get para obtener post

--- Cliente/cliente/src/services/postsServices.js
+++ Cliente/cliente/src/services/postsServices.js
@@ -18,4 +18,23 @@ async function getHomePosts(page){
     }
 }
 
-export default getHomePosts;
+async function getPostById(id){
+    const settings = {
+        method: 'GET',
+        headers: new Headers({
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            //'Authorization': 'Bearer ' + token
+        }),
+    }
+
+    try {
+        const response = await fetch('http://localhost:4000/post/' + id, settings);
+        const json = await response.json();
+        return json;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export {getHomePosts, getPostById}
