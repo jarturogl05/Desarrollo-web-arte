fbbbddfec7f2a27ac8946f66ae874e61e1d49e5e
jarturogl05 <jarturogl05@gmail.com>
2021-01-16 22:49:01 -0600

    creacion de post con token

--- Servidor/src/controllers/post-controller.js
+++ Servidor/src/controllers/post-controller.js
@@ -1,38 +1,67 @@
 const Post = require('../mongo/models/post');
+const jwt = require('jwt-simple');
+const Users = require('../mongo/models/user.js');
+const tokenService = require('./token-service');
+
 
 const {resizeImageBuffer, applySmartCrop} = require('../images-helper/imageReziser');
 const {uploadImageMiniature, uploadImagePost, uploadImageProfile, createImageName} = require('../images-helper/image-uploader')
 const URLBlob = 'https://imageswebart.blob.core.windows.net/'
 
-const createPost = async(req, res) =>{
-    const{autorId, description, name, workType, tags} = req.body;
-    const newtags = tags.replace(/\s/g, '').split(",");
-    const blobImageName = createImageName(name);
-    const blobThumbnailName = 'thumbnail' + createImageName(name);
-    const URLImage = URLBlob + 'imagenes/' + blobImageName;
-    const URLThumbnail = URLBlob + 'miniaturas/' + blobThumbnailName;
+const createPost = async (req, res) => {
+  if (!req.headers.authorization) {
+    res.status(403).send("Access Forbidden");
+  } else {
+    const { description, name, workType, tags } = req.body;
+    try {
+      const tokenCode = req.headers.authorization;
+      const token = tokenCode.split(" ")[1];
+      const tokenResult = await tokenService.decodeToken(token);
+      if (tokenResult.message === "Token expired") {
+        res.status(500).send({ status: "ERROR", message: "error" });
+      } else {
+        const username = tokenResult;
+        const user = await Users.find({ username: username });
+        const autorId = user[0]._id;
 
-    try{
-        await Post.create({
+        const newtags = tags.replace(/\s/g, "").split(",");
+        const blobImageName = createImageName(name);
+        const blobThumbnailName = "thumbnail" + createImageName(name);
+        const URLImage = URLBlob + "imagenes/" + blobImageName;
+        const URLThumbnail = URLBlob + "miniaturas/" + blobThumbnailName;
+
+        try {
+          await Post.create({
             autorId,
             name,
             description,
             workType,
-            tags:newtags,
+            tags: newtags,
             URLImage,
             URLThumbnail,
-        })
+          });
 
-        var bufferResize = await resizeImageBuffer(req.file.buffer);
-        var bufferThumbnail = await applySmartCrop(bufferResize, '', 512, 512);
-        await uploadImagePost(bufferResize, blobImageName);
-        await uploadImageMiniature(bufferThumbnail, blobThumbnailName)
-        res.status(200).send({message: 'Registered Post'})
-    }catch(error){
-        console.log(error);
-        res.status(500).send({status:'ERROR', message: 'error'});
+          var bufferResize = await resizeImageBuffer(req.file.buffer);
+          var bufferThumbnail = await applySmartCrop(
+            bufferResize,
+            "",
+            512,
+            512
+          );
+          await uploadImagePost(bufferResize, blobImageName);
+          await uploadImageMiniature(bufferThumbnail, blobThumbnailName);
+          res.status(200).send({ message: "Registered Post" });
+        } catch (error) {
+          console.log(error);
+          res.status(500).send({ status: "ERROR", message: "error" });
+        }
+      }
+    } catch (e) {
+      console.log(e);
+      res.status(500).send({ status: "ERROR", message: "error" });
     }
-}
+  }
+};
 
 const getPost = async (req, res) =>{
     const postId = req.params.id;
