3f2002bd58839329d5ddfed389b8af9498adcd36
jarturogl05 <jarturogl05@gmail.com>
2021-01-17 16:25:20 -0600

    logout

--- Cliente/cliente/src/componens/dropdown-navbar/dropdown.js
+++ Cliente/cliente/src/componens/dropdown-navbar/dropdown.js
@@ -1,20 +1,30 @@
-import React, { useState } from 'react';
-import './dropdown.css';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+import "./dropdown.css";
 
 function Dropdown() {
-  const [click, setClick] = useState(false)
-  ;
+  const [click, setClick] = useState(false);
+  const history = useHistory();
+
   const handleClick = () => setClick(!click);
+
+  const logOut = (e) => {
+    e.preventDefault();
+    localStorage.clear();
+    history.replace("/login");
+  };
 
   return (
     <>
       <ul
         onClick={handleClick}
-        className={click ? "dropdown-menu clicked" : "dropdown-menu"}>
+        className={click ? "dropdown-menu clicked" : "dropdown-menu"}
+      >
         <li>
           <a href="/messages">Messages</a>
           <a href="/settings">Settings</a>
-          <a>Logout</a>
+          <a onClick={(e) => logOut(e)}>Logout</a>
         </li>
       </ul>
     </>
