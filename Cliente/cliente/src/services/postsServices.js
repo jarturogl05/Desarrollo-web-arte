19b9905e8efd0ce04876f7185333b12c36d8a559
jarturogl05 <jarturogl05@gmail.com>
2021-01-19 15:10:57 -0600

    obtener galeria de imagenes de usuario

--- Cliente/cliente/src/services/postsServices.js
+++ Cliente/cliente/src/services/postsServices.js
@@ -56,4 +56,24 @@ async function getPostByUser(autorId, page){
     }
 }
 
-export {getHomePosts, getPostById, getPostByUser}
+async function getPostByUserName(autorName, page){
+    const settings = {
+        method: 'GET',
+        headers: new Headers({
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            //'Authorization': 'Bearer ' + token
+        }),
+    }
+
+    try {
+        const response = await fetch('http://localhost:4000/autorPostsName/' + autorName + '/' + page, settings);
+        const json = await response.json();
+        return json;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+export {getHomePosts, getPostById, getPostByUser, getPostByUserName}
